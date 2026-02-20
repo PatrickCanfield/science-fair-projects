ae09cac8a8f3d6ec1d0c751e12a63233c40b6f35
ChristopherCanfield <cdcanfield@gmail.com>
2026-02-20 16:52:42 -0500

    [Patrick] Added VOCs & CO2
    Plus some formatting changes.

--- 2026/bf-code.py
+++ 2026/bf-code.py
@@ -45,6 +45,7 @@ def aqiname(value):
     else:
         return "Error"
 
+
 display_bus = I2CDisplayBus(i2c, device_address=0x3D, reset=oled_reset)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=WIDTH, height=HEIGHT)
 
@@ -53,26 +54,36 @@ screen = displayio.Group()
 display.root_group = screen
 
 color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
-color_palette = displayio.Palette(1)
+color_palette = displayio.Palette(2)
 color_palette[0] = 0x000000
+color_palette[1] = 0xFFFFFF
 
 bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
 screen.append(bg_sprite)
 
-header_text_area = label.Label(terminalio.FONT, text="-- Particulates --", color=0xFFFFFF, x=0, y=4)
+header_text_area = label.Label(terminalio.FONT, text="  -- Particulates --", x=0, y=4)
 screen.append(header_text_area)
 
-pm10_text_area = label.Label(terminalio.FONT, text="", color=0xFFFFFF, x=0, y=20)
+pm10_text_area = label.Label(terminalio.FONT, text="", x=0, y=20)
 screen.append(pm10_text_area)
 
-pm25_text_area = label.Label(terminalio.FONT, text="", color=0xFFFFFF, x=0, y=32)
+pm25_text_area = label.Label(terminalio.FONT, text="", x=0, y=32)
 screen.append(pm25_text_area)
 
-pm100_text_area = label.Label(terminalio.FONT, text="", color=0xFFFFFF, x=0, y=44)
+pm100_text_area = label.Label(terminalio.FONT, text="", x=0, y=44)
 screen.append(pm100_text_area)
 
-aqi_text_area = label.Label(terminalio.FONT, text="", color=0xFFFFFF, x=0, y=56)
+aqi_text_area = label.Label(terminalio.FONT, text="", x=0, y=56)
 screen.append(aqi_text_area)
+
+voc_text_area = label.Label(terminalio.FONT, text="", x=68, y=20)
+screen.append(voc_text_area)
+
+co2_text_area = label.Label(terminalio.FONT, text="", x=68, y=32)
+screen.append(co2_text_area)
+
+for i in range(20,68,12):
+    screen.append(label.Label(terminalio.FONT, text="|", x=64, y=i))
 
 while True:
     time.sleep(1)
@@ -83,10 +94,12 @@ while True:
         print("Unable to read from sensor, retrying...")
         continue
 
-    aqi_text_area.text = str.format("AQI: {}", aqiname(ens.AQI))
-    pm10_text_area.text = str.format("PM 1.0:  {}", aqdata["pm10 standard"])
-    pm25_text_area.text = str.format("PM 2.5:  {}", aqdata["pm25 standard"])
-    pm100_text_area.text = str.format("PM 10.0: {}", aqdata["pm100 standard"])
+    aqi_text_area.text = str.format("AQI:{}", ens.AQI)
+    voc_text_area.text = str.format("VOC:{}", ens.TVOC)
+    co2_text_area.text = str.format("CO2:{}", ens.eCO2)
+    pm10_text_area.text = str.format("PM 1.0:{}", aqdata["pm10 standard"])
+    pm25_text_area.text = str.format("PM 2.5:{}", aqdata["pm25 standard"])
+    pm100_text_area.text = str.format("PM 10: {}", aqdata["pm100 standard"])
 
     print("---------------------------------------")
     print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
